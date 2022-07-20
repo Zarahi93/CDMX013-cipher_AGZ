c2c16684cb2789e1427b5b8b431b25f1e8c2bc6b
Zarahi93 <adahi.g.z@hotmail.com>
2022-07-20 00:01:26 -0500

    Inicio de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,13 @@
+function cifradoCesar() {
+  let entrada = document.getElementById('paCifrar').value;
+  let solucion= '';
+  let shiftInput = parseInt(document.getElementById('shiftInput').value);
+  
+
+
+}
 const cipher = {
   // ...
 };
-
+(x-65+n)%26+65
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let botonCifrado = document.getElementById('cb'); //boton para cifrar
+let salida = document.getElementById('resCif'); // donde la contraseña cifrada aparecera
+let entrada = document.getElementById('paCifrar');// el texto a cifrar
+let inputs= [entrada,salida];
+
+inputs.forEach( input => {
+    input.oninput = () => {
+        input.value.toUpperCase()//transforma todas las letras a mayuscula
+    }
+})
