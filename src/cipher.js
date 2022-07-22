d8ef6af387e5b58784e1d0c6f837ddaa9e6bfe2e
Zarahi93 <adahi.g.z@hotmail.com>
2022-07-21 22:03:15 -0500

    Mi desastre

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,20 @@
-function cifradoCesar() {
+const cipher = function cifradoCesar() {
   let entrada = document.getElementById('paCifrar').value;
   let solucion= '';
   let shiftInput = parseInt(document.getElementById('shiftInput').value);
-  
 
-
-}
-const cipher = {
-  // ...
+  for (let i = 0; i < entrada.length; i++) {
+   let ascii_num = entrada[i].charCodeAt()//regresa el codigo para cada letra
+   let sum = ascii_num + shiftInput
+   sum >= 65 && sum <= 90 ? solucion += String.fromCharCode(sum) : sum > 90 ? solucion += String.fromCharCode(65+(sum & 91)): solucion += entrada [i]
+  }
+salida.value = solucion
 };
-(x-65+n)%26+65
+
+botonCifrado.addEventListener('click',cipher)
+
 export default cipher;
+
+cipher.encode(offset0, entrada)
+
+cipher.decode(offset1, )
