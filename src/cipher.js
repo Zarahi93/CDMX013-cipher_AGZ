603371928f66cadae0e6a783b704f2e06fc8f90a
Zarahi93 <adahi.g.z@hotmail.com>
2022-07-25 12:06:49 -0500

    Despues del OH

--- src/cipher.js
+++ src/cipher.js
@@ -1,56 +1,12 @@
-
 const cipher = {
-    function inicio(){
-        document.getElementById("entrada").addEventListener("keyup", function(){
-            this.value = this.value.toUpperCase();
-        }, true);
-        
-        document.getElementById("botonCifrar").addEventListener("click",function(){  
-            let texto = document.getElementById("entrada").value;
-            let desplazamiento = document.getElementById("desplazamiento").value;               
-            document.getElementById("salida").value = cifrar2(texto, desplazamiento);
-        },true);
-        document.getElementById("botonDescifrar").addEventListener("click",function(){  
-            let texto = document.getElementById("entrada").value;
-            let desplazamiento = document.getElementById("desplazamiento").value;                               
-            document.getElementById("salida").value = descifrar(texto, desplazamiento);
-        },true);
-    }
-    
-    function cifrar(texto, desplazamiento) {
-        if (!texto) 
-            return ''; 
-        const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        desplazamiento = (desplazamiento % 26 + 26) % 26; 
-        return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) + desplazamiento) % 26]);
-    }
-    
-    function descifrar(texto, desplazamiento) {
-        if (!texto) 
-            return ''; 
-        const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        desplazamiento = (desplazamiento % 26 - 26) % 26; 
-        return texto.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - desplazamiento) % 26]);
-    }
-    
-    function cifrar2(texto, desplazamiento) {
-        let resultado='';
-        let letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        desplazamiento = (desplazamiento % 26 + 26) % 26; 
-        
-        if (texto){
-            for (let i=0; i<texto.length; ++i){
-                if (letras.indexOf(texto[i])!=-1)
-                { 
-                    let posicion=((letras.indexOf(texto[i])+desplazamiento) % 26);
-                    resultado+=letras[posicion];
-                }
-                else
-                    resultado+=texto[i]; 
-            }
-        }
-        return resultado;
-    }
-}
-export default cipher;
+    encode: function (offset,string){
 
+    },
+
+    decode: function (offset,string){
+
+    }
+  };
+  
+  export default cipher;
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let string = document.getElementById('entrada');
+let offset = document.getElementById('desplazamiento');
+let respuesta = document.getElementById('salida');
+let cifrar = document.getElementById('botonCifrar');
+let descifrar = document.getElementById('botonDescifrar');
+let reset = document.getElementById('Limpiar');
+
+cifrar.addEventListener('clik', function() {
+  cipher.encode(offset.value, string.value)
+
+});
+
+descifrar.addEventListener('click', function (){
+  cipher.decode(offset.value, string.value)
+});
