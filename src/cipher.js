c49ee81bfa32238518876c3d5583ea664126384b
Zarahi93 <adahi.g.z@hotmail.com>
2022-07-29 12:57:45 -0500

    El descifrado no sirve

--- src/cipher.js
+++ src/cipher.js
@@ -27,6 +27,31 @@ encode: (offset,contr)=> {
         return respuesta;
     }
 
-//decode: function (offset,contr) {},
+decode: (offset,contr)=> {
+    console.log(contr)
+    console.log(offset)
+        let respuesta=''; //Se declara la respuesta
+        for (let i=0; i<contr.length;i++){
+            let letra= contr[i];
+            console.log(letra)
+            if (letra.match(/[a-z]/i) || letra.match(/[A-Z]/i)){
+                const codigo = contr.charCodeAt(i);//Obtener el codigo ASCII de la letra
+               console.log(codigo)
+                //Para mayusculas
+                if(codigo >= 65 && codigo <= 90){
+                    letra= String.fromCharCode( ( (codigo - 65 - Number(offset) ) % 26 ) + 65 );
+                }
+                //Para minusculas
+                else if (codigo>=97 && codigo<=122){
+                    letra= String.fromCharCode( ( (codigo - 97 - Number(offset) ) % 26 ) + 97 );
+                }
+                console.log(letra)
+            }
+            respuesta += letra;
+            console.log(respuesta)
+
+        }
+        return respuesta;   
+ }
 };
  export default cipher;
