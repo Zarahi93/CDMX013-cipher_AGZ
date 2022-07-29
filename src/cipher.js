a22f4d78cc0b39d0308d720a0c2f62cea3897f08
Zarahi93 <adahi.g.z@hotmail.com>
2022-07-29 13:26:36 -0500

    Ya funcionan todos los botones

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,11 @@
 const cipher = {
 
     encode: (offset, contr) => {
-        //console.log(contr)
-        //console.log(offset)
         let respuesta = ''; //Se declara la respuesta
         for (let i = 0; i < contr.length; i++) {
             let letra = contr[i];
-            //console.log(letra)
             if (letra.match(/[a-z]/i) || letra.match(/[A-Z]/i)) {
                 const codigo = contr.charCodeAt(i);//Obtener el codigo ASCII de la letra
-                //console.log(codigo)
                 //Para mayusculas
                 if (codigo >= 65 && codigo <= 90) {
                     letra = String.fromCharCode(((codigo - 65 + Number(offset)) % 26) + 65);
@@ -18,11 +14,8 @@ const cipher = {
                 else if (codigo >= 97 && codigo <= 122) {
                     letra = String.fromCharCode(((codigo - 97 + Number(offset)) % 26) + 97);
                 }
-                //console.log(letra)
             }
             respuesta += letra;
-            //console.log(respuesta)
-
         }
         return respuesta;
     },
