4cd55a011cacf229dd8349b079ba5feaa79a910b
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2022-08-03 19:04:26 -0500

    Comentarios

--- src/cipher.js
+++ src/cipher.js
@@ -6,19 +6,19 @@ const cipher = {
         }
         let respuesta = ''; //Se declara la variable donde se guardara la respuesta
         for (let i = 0; i < contr.length; i++) {
-            let letra = contr[i];
+            let letra = contr[i];//Separa cada letra una por una
             if (letra.match(/[a-z]/i) || letra.match(/[A-Z]/i)) {
                 const codigo = contr.charCodeAt(i);//Obtener el codigo ASCII de la letra
                 //Para mayusculas
-                if (codigo >= 65 && codigo <= 90) {
-                    letra = String.fromCharCode(((codigo - 65 + Number(offset)) % 26) + 65);
+                if (codigo >= 65 && codigo <= 90) {//Revisa que el codigo ASCII obtenido de la letra mayúscula este en el rango de las letras mayuculas
+                    letra = String.fromCharCode(((codigo - 65 + Number(offset)) % 26) + 65);//reasigna el valor a la letra ya movida segun el despalazamiento
                 }
                 //Para minusculas
-                else if (codigo >= 97 && codigo <= 122) {
+                else if (codigo >= 97 && codigo <= 122) {//Revisa que el codigo ASCII obtenido de la letra minúscula este en el rango de las letras minúsculas
                     letra = String.fromCharCode(((codigo - 97 + Number(offset)) % 26) + 97);
                 }
             }
-            respuesta += letra;
+            respuesta += letra;//Concatena todas las letras despues de que fueron cifradas una por una.
         }
         return respuesta;
     },
@@ -41,7 +41,7 @@ const cipher = {
                     letra = String.fromCharCode(((codigo - 122 - Number(offset)) % 26) + 122);
                 }
             }
-            respuesta += letra;
+            respuesta += letra;//Concatena todas las letras despues de que fueron descifradas una por una.
         }
         return respuesta;
     }
